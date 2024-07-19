9dd8f98e8681b08dcabc3baa7cca1a5b91c8f1b0
SaraCarlenis <sara.hurtado.corte@hotmail.com>
2024-07-19 19:09:50 -0300

    final

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,4 @@
-import br.com.dio.desafio.dominio.Curso;
-import br.com.dio.desafio.dominio.Mentoria;
+import br.com.dio.desafio.dominio.*;
 
 import java.time.LocalDate;
 
@@ -12,7 +11,7 @@ public class Main {
 
         Curso curso2 = new Curso();
         curso2.setTitulo("Curso js");
-        curso2.setDescricao("Desafio dos primeiros passos em js");
+        curso2.setDescricao("Desafio js");
         curso2.setCargaHoraria(10);
 
         Mentoria mentoria = new Mentoria();
@@ -24,6 +23,36 @@ public class Main {
         System.out.println(curso1);
         System.out.println(curso2);
         System.out.println(mentoria);
+
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devSara = new Dev();
+        devSara.setNome("Sara");
+        devSara.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Inscritos" + devSara.getConteudosInscritos());
+        devSara.progredir();
+        System.out.println("Conteúdos Inscritos" + devSara.getConteudosInscritos());
+        devSara.progredir();
+        System.out.println("Conteúdos Inscritos" + devSara.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devSara.getConteudosConcluidos());
+        System.out.println("XP:" + devSara.calcularTotalXp());
+
+
+        Dev devVinicius = new Dev();
+        devVinicius.setNome("Vinicius");
+        devVinicius.inscreverBootcamp(bootcamp);
+        System.out.println("Conteúdos Incritos" + devVinicius.getConteudosInscritos());
+        devVinicius.progredir();
+        devVinicius.progredir();
+        devVinicius.progredir();
+        System.out.println("Conteúdos Inscritos" + devSara.getConteudosInscritos());
+        System.out.println("Conteúdos Concluídos" + devVinicius.getConteudosConcluidos());
+        System.out.println("XP:" + devVinicius.calcularTotalXp());
 
     }
 }
--- src/br/com/dio/desafio/dominio/Conteudo.java
+++ src/br/com/dio/desafio/dominio/Conteudo.java
@@ -1,6 +1,6 @@
 package br.com.dio.desafio.dominio;
 
-public class Conteudo {
+public abstract class Conteudo {
 
    protected static final double XP_PADRAO = 10d;
 
@@ -9,5 +9,19 @@ public class Conteudo {
 
    public abstract double calcularXp();
 
+   public String getDescricao() {
+      return descricao;
+   }
 
+   public void setDescricao(String descricao) {
+      this.descricao = descricao;
+   }
+
+   public String getTitulo() {
+      return titulo;
+   }
+
+   public void setTitulo(String titulo) {
+      this.titulo = titulo;
+   }
 }
